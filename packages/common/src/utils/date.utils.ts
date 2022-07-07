22f9b5b419af5beff5d28bbe69e0cfc76e2e4da9
Traian Anghel <tanghel@live.com>
2022-07-07 11:44:29 +0300

    added create utc endpoint when creating date

--- packages/common/src/utils/date.utils.ts
+++ packages/common/src/utils/date.utils.ts
@@ -1,5 +1,17 @@
 export class DateUtils {
+  static createUTC(year: number, month?: number, day?: number, hours?: number, minutes?: number, seconds?: number, milliseconds?: number): Date {
+    const date = new Date();
+    date.setUTCFullYear(year, (month ?? 1) - 1, day ?? 1);
+    date.setUTCHours(hours ?? 0, minutes ?? 0, seconds ?? 0, milliseconds ?? 0);
+
+    return date;
+  }
+
   static create(year: number, month?: number, day?: number, hours?: number, minutes?: number, seconds?: number, milliseconds?: number): Date {
-    return new Date(year, (month ?? 1) - 1, day ?? 1, hours ?? 0, minutes ?? 0, seconds ?? 0, milliseconds ?? 0);
+    const date = new Date();
+    date.setFullYear(year, (month ?? 1) - 1, day ?? 1);
+    date.setHours(hours ?? 0, minutes ?? 0, seconds ?? 0, milliseconds ?? 0);
+
+    return date;
   }
 }
