984294dbb0a1bb320137ec845eb3ada5aca88d56
Vlad-Adrian Bucur <vladadrianbucur@gmail.com>
2022-08-09 11:33:30 +0300

    Pending Executor Fix

--- packages/common/src/utils/pending.executer.ts
+++ packages/common/src/utils/pending.executer.ts
@@ -1,29 +1,17 @@
-import * as crypto from 'crypto-js';
-
 export class PendingExecuter {
-  private dictionary: Record<string, Promise<any>> = {};
-
-  constructor(
-    private readonly executer?: (value: any) => Promise<any>,
-  ) { }
+  private readonly dictionary: Record<string, Promise<any>> = {};
 
   async execute<TOut>(
-    value: any,
-    executer?: (value: any) => Promise<TOut>,
+    key: string,
+    executer: () => Promise<TOut>,
   ): Promise<TOut> {
-    const localExecuter = executer ?? this.executer;
-    if (!localExecuter) {
-      throw new Error('No Executor passed.');
-    }
-    const key = crypto.MD5(JSON.stringify(value)).toString();
-
     const pendingRequest = this.dictionary[key];
     if (pendingRequest) {
       return await pendingRequest;
     }
 
     try {
-      return await (this.dictionary[key] = localExecuter(value));
+      return await (this.dictionary[key] = executer());
     } finally {
       delete this.dictionary[key];
     }
