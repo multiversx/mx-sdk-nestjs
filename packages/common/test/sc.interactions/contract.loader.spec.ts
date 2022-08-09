2b546e850f21f4fda2b9463ea7a4329343bc5d87
Alexandru Pislariu <57287326+AlexandruPislariu@users.noreply.github.com>
2022-08-09 11:51:06 +0300

    Support for smart contract interactions (query, tx generator) (#24)
    * Support for smart contract interactions (query, tx generator)
    
    * add tests for loader and tx generator

---
+++ packages/common/test/sc.interactions/contract.loader.spec.ts
@@ -0,0 +1,42 @@
+import { Address, SmartContract } from "@elrondnetwork/erdjs/out";
+import { ContractLoader } from "src/sc.interactions/contract.loader";
+
+describe("Contract loader", () => {
+  const CONTRACT_ADDRESS = "erd1qqqqqqqqqqqqqpgqkdz87p5raf5tsyv66ld8cu49nf2dqpp9d8ss36ltf2";
+  const ABI_PATH: string = 'src/abis/metabonding.abi.json';
+  const CONTRACT_INTERFACE: string = 'Metabonding';
+  const contractLoader: ContractLoader = new ContractLoader(ABI_PATH, CONTRACT_INTERFACE);
+
+  it('Should load metabonding contract', async () => {
+    const contract: SmartContract = await contractLoader.getContract(CONTRACT_ADDRESS);
+
+    expect(contract.getAddress().bech32()).toStrictEqual(CONTRACT_ADDRESS);
+  });
+
+  it('Should load contract from memory once', async () => {
+    const cLoader = new ContractLoader(ABI_PATH, CONTRACT_INTERFACE);
+
+    const loadSpy = jest
+      .spyOn(ContractLoader.prototype as any, 'load')
+      // eslint-disable-next-line require-await
+      .mockImplementation(jest.fn(async () => new SmartContract({ address: new Address(CONTRACT_ADDRESS) })));
+
+    await cLoader.getContract(CONTRACT_ADDRESS);
+
+    expect(loadSpy).toHaveBeenCalled();
+    expect(loadSpy.mock.calls.length).toStrictEqual(1);
+
+    await cLoader.getContract(CONTRACT_ADDRESS);
+    expect(loadSpy.mock.calls.length).toStrictEqual(1);
+  });
+
+  it('Should throw error, abi path is invalid', async () => {
+    const cLoader = new ContractLoader("./invalid-path", CONTRACT_INTERFACE);
+
+    await expect(cLoader.getContract(CONTRACT_ADDRESS)).rejects.toThrow(Error);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+});
