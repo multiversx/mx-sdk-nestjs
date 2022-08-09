ed5e2f9ef6a9e6c06053ce08abb027373f6fc3c2
AlexandruPislariu <alexandru.pislariu@elrond.com>
2022-08-09 11:23:14 +0300

    add tests for loader and tx generator

---
+++ packages/common/test/sc.interactions/contract.transaction.generator.spec.ts
@@ -0,0 +1,35 @@
+import { AccountOnNetwork, ApiNetworkProvider, NetworkConfig } from "@elrondnetwork/erdjs-network-providers/out";
+import { Address, ContractFunction, IAddress, Interaction, SmartContract } from "@elrondnetwork/erdjs/out";
+import { ContractTransactionGenerator } from "src/sc.interactions/contract.transaction.generator";
+
+const TEST_ADDRESS = "erd1wtm3yl58vcnj089lqy3tatkdpwklffh4pjnf27zwsa2znjyk355sutafqh";
+describe("Contract transaction generator", () => {
+  it('Should set transaction nonce', async () => {
+    const cTxGenerator = new ContractTransactionGenerator(new ApiNetworkProvider("some-url"));
+
+    const getAccountSpy = jest
+      .spyOn(ApiNetworkProvider.prototype, "getAccount")
+      // eslint-disable-next-line require-await
+      .mockImplementation(jest.fn(async (_: IAddress) => new AccountOnNetwork({ nonce: 10 })));
+
+    const getNetworkConfigSpy = jest
+      .spyOn(ApiNetworkProvider.prototype, "getNetworkConfig")
+      // eslint-disable-next-line require-await
+      .mockImplementation(jest.fn(async () => new NetworkConfig()));
+
+    const contract = new SmartContract({ address: new Address(TEST_ADDRESS) });
+    const dummyFunction = new ContractFunction("dummy");
+    const interaction = new Interaction(contract, dummyFunction, []);
+
+    interaction
+      .withNonce(7)
+      .withGasLimit(20000000);
+
+    const tx = await cTxGenerator.createTransaction(interaction, new Address(TEST_ADDRESS));
+
+    expect(getAccountSpy).toHaveBeenCalled();
+    expect(getNetworkConfigSpy).toHaveBeenCalled();
+
+    expect(tx.getNonce()).toStrictEqual(10);
+  });
+});
