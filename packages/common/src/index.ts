3f39b1eb2cc6efdf3a182d2941b7145d6ed23cd0
Traian Anghel <tanghel@live.com>
2022-08-11 13:14:18 +0300

    removed parse int pipe

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -32,7 +32,6 @@ export * from './pipes/parse.hash.pipe';
 export * from './pipes/parse.bool.pipe';
 export * from './pipes/parse.enum.pipe';
 export * from './pipes/parse.enum.array.pipe';
-export * from './pipes/parse.int.pipe';
 export * from './pipes/parse.nft.pipe';
 export * from './pipes/parse.token.pipe';
 export * from './pipes/parse.token.or.nft.pipe';
