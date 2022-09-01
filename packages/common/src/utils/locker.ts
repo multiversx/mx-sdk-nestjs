caa05804aee5bc3e1727dbdd163c23f0a26f0bb8
Gabriel Matei <gabrielvmatei@gmail.com>
2022-09-01 14:38:16 +0300

    return locker result

--- packages/common/src/utils/locker.ts
+++ packages/common/src/utils/locker.ts
@@ -7,12 +7,12 @@ import { PerformanceProfiler } from "./performance.profiler";
 export class Locker {
   private static lockArray: string[] = [];
 
-  static async lock(key: string, func: () => Promise<void>, log: boolean = false) {
+  static async lock(key: string, func: () => Promise<void>, log: boolean = false): Promise<LockResult> {
     const logger = new Logger('Lock');
 
     if (Locker.lockArray.includes(key)) {
       logger.log(`${key} is already running`);
-      return;
+      return LockResult.ALREADY_RUNNING;
     }
 
     Locker.lockArray.push(key);
@@ -29,6 +29,8 @@ export class Locker {
       cpuProfiler?.stop(log ? `Running ${key}` : undefined);
 
       MetricsService.setJobResult(key, 'success', profiler.duration);
+
+      return LockResult.SUCCESS;
     } catch (error) {
       logger.error(`Error running ${key}`);
       logger.error(error);
@@ -37,8 +39,16 @@ export class Locker {
       cpuProfiler?.stop(log ? `Running ${key}` : undefined);
 
       MetricsService.setJobResult(key, 'error', profiler.duration);
+
+      return LockResult.ERROR;
     } finally {
       Locker.lockArray.remove(key);
     }
   }
+}
+
+export enum LockResult {
+  SUCCESS = 'success',
+  ALREADY_RUNNING = 'alreadyRunning',
+  ERROR = 'error'
 }
