cfa774a156bdf1dc4b45b5de65f0a03699d4bc1f
Vlad-Adrian Bucur <vladadrianbucur@gmail.com>
2022-10-07 14:51:36 +0300

    Fixes

--- packages/common/src/common/caching/redis-cache/options.ts
+++ packages/common/src/common/caching/redis-cache/options.ts
@@ -5,11 +5,29 @@ export class RedisCacheModuleOptions {
   port?: number | undefined;
   username?: string | undefined;
   password?: string | undefined;
+  sentinelUsername?: string | undefined;
+  sentinelPassword?: string | undefined;
   sentinels?: Array<{ host: string; port: number }> | undefined;
   connectTimeout?: number | undefined;
-  config = {
-    url: '',
+  config: {
+    url: string,
   };
+
+  constructor(
+    options: Omit<RedisCacheModuleOptions, 'config'>,
+  ) {
+    this.host = options.host;
+    this.port = options.port;
+    this.username = options.username;
+    this.password = options.password;
+    this.sentinelUsername = options.sentinelUsername;
+    this.sentinelPassword = options.sentinelPassword;
+    this.sentinels = options.sentinels;
+    this.connectTimeout = options.connectTimeout;
+    this.config = {
+      url: '',
+    };
+  }
 }
 
 export interface RedisCacheModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
