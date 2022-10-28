a3b34a331e5827e1cfda8122817b6265c6b3575a
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2022-10-28 09:29:56 +0300

    add fully independent redis client module

--- packages/common/src/common/caching/redis-cache/redis-cache.module.ts
+++ packages/common/src/common/caching/redis-cache/redis-cache.module.ts
@@ -1,9 +1,8 @@
-import Redis from 'ioredis';
 import { DynamicModule, Module } from '@nestjs/common';
 import { RedisCacheService } from './redis-cache.service';
 import { RedisCacheModuleOptions, RedisCacheModuleAsyncOptions } from './options';
 import { MetricsModule } from '../../../common/metrics/metrics.module';
-import { REDIS_CLIENT_TOKEN, REDIS_OPTIONS_TOKEN } from './entities/common.constants';
+import { RedisModule } from '../../redis/redis.module';
 
 @Module({})
 export class RedisCacheModule {
@@ -11,13 +10,10 @@ export class RedisCacheModule {
     return {
       module: RedisCacheModule,
       imports: [
+        RedisModule.forRoot(options),
         MetricsModule,
       ],
       providers: [
-        {
-          provide: REDIS_CLIENT_TOKEN,
-          useValue: new Redis(options.config),
-        },
         RedisCacheService,
       ],
       exports: [
@@ -30,20 +26,10 @@ export class RedisCacheModule {
     return {
       module: RedisCacheModule,
       imports: [
+        RedisModule.forRootAsync(asyncOptions),
         MetricsModule,
-        ...(asyncOptions.imports || []),
       ],
       providers: [
-        {
-          inject: asyncOptions.inject || [],
-          provide: REDIS_OPTIONS_TOKEN,
-          useFactory: asyncOptions.useFactory,
-        },
-        {
-          inject: [REDIS_OPTIONS_TOKEN],
-          provide: REDIS_CLIENT_TOKEN,
-          useFactory: (factoryOptions: RedisCacheModuleOptions) => new Redis(factoryOptions.config),
-        },
         RedisCacheService,
       ],
       exports: [
