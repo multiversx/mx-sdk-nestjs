ef7324f5512c36e1d3d36ef92179cb191ed0ffb0
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-12-14 14:28:49 +0200

    fix api utils

--- packages/common/src/utils/api.utils.ts
+++ packages/common/src/utils/api.utils.ts
@@ -1,5 +1,5 @@
 export class ApiUtils {
-  static mergeObjects<T>(obj1: T, obj2: any) {
+  static mergeObjects<T extends object>(obj1: T, obj2: any) {
     for (const key of Object.keys(obj2)) {
       if (key in obj1) {
         // @ts-ignore
