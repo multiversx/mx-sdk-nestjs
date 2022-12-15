321b2e4a69e18294fb1dc3acf85106ab22063dc9
Gabriel Matei <gabrielvmatei@gmail.com>
2022-12-15 12:49:00 +0200

    add support for ElrondCaching in NativeAuth and fix issues

--- packages/common/src/common/config/erdnest.config.service.ts
+++ packages/common/src/common/config/erdnest.config.service.ts
@@ -1,5 +1,9 @@
+import { NativeAuthGuardOptions } from "../../guards/native.auth.guard";
+
 export interface ErdnestConfigService {
   getSecurityAdmins(): string[];
 
   getJwtSecret(): string;
+
+  getNativeAuthGuardOptions(): NativeAuthGuardOptions | undefined;
 }
--- packages/common/src/guards/native.auth.guard.ts
+++ packages/common/src/guards/native.auth.guard.ts
@@ -1,15 +1,29 @@
 import { NativeAuthServer } from '@elrondnetwork/native-auth-server';
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, Optional, Inject } from '@nestjs/common';
 import { OriginLogger } from '../utils/origin.logger';
 import { CachingService } from '../common/caching/caching.service';
+import { ElrondCachingService } from '../common/caching/elrond-caching/elrond-caching.service';
+import { ERDNEST_CONFIG_SERVICE } from '../utils/erdnest.constants';
+import { ErdnestConfigService } from '../common/config/erdnest.config.service';
+
+export interface NativeAuthGuardOptions {
+  apiUrl?: string;
+  acceptedHosts?: string[];
+  maxExpirySeconds?: number;
+}
 
 @Injectable()
 export class NativeAuthGuard implements CanActivate {
   private readonly logger = new OriginLogger(NativeAuthGuard.name);
   private readonly authServer: NativeAuthServer;
 
-  constructor(cachingService: CachingService) {
+  constructor(
+    @Inject(ERDNEST_CONFIG_SERVICE) erdnestConfigService: ErdnestConfigService,
+    @Optional() cachingService?: CachingService,
+    @Optional() elrondCachingService?: ElrondCachingService,
+  ) {
     this.authServer = new NativeAuthServer({
+      ...erdnestConfigService.getNativeAuthGuardOptions(),
       cache: {
         getValue: async function <T>(key: string): Promise<T | undefined> {
           if (key === 'block:timestamp:latest') {
@@ -18,27 +32,44 @@ export class NativeAuthGuard implements CanActivate {
             return new Date().getTime() / 1000;
           }
 
-          return await cachingService.getCache<T>(key);
+          if (elrondCachingService) {
+            return await elrondCachingService.get<T>(key);
+          }
+
+          if (cachingService) {
+            return await cachingService.getCache<T>(key);
+          }
+
+          throw new Error('CachingService or ElrondCachingService is not available in the context');
         },
         setValue: async function <T>(key: string, value: T, ttl: number): Promise<void> {
-          await cachingService.setCache(key, value, ttl);
+          if (elrondCachingService) {
+            return await elrondCachingService.set<T>(key, value, ttl);
+          }
+
+          if (cachingService) {
+            await cachingService.setCache<T>(key, value, ttl);
+            return;
+          }
+
+          throw new Error('CachingService or ElrondCachingService is not available in the context');
         },
       },
     });
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-
-    const host = new URL(request.headers['origin']).hostname;
-
-    const authorization: string = request.headers['authorization'];
-    if (!authorization) {
-      return false;
-    }
-    const jwt = authorization.replace('Bearer ', '');
-
     try {
+      const request = context.switchToHttp().getRequest();
+
+      const host = new URL(request.headers['origin']).hostname;
+
+      const authorization: string = request.headers['authorization'];
+      if (!authorization) {
+        return false;
+      }
+      const jwt = authorization.replace('Bearer ', '');
+
       const userInfo = await this.authServer.validate(jwt);
       if (userInfo.host !== host) {
         this.logger.error(`Invalid host '${userInfo.host}'. should be '${host}'`);
@@ -51,10 +82,12 @@ export class NativeAuthGuard implements CanActivate {
       request.res.set('X-Native-Auth-Timestamp', Math.round(new Date().getTime() / 1000));
 
       request.nativeAuth = userInfo;
+
       return true;
     } catch (error) {
       this.logger.error(error);
-      throw new UnauthorizedException();
+
+      return false;
     }
   }
 }
