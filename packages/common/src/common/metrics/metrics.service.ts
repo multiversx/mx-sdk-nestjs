0278fb99533cd6f06050d702eda4ba5a238ce911
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2022-12-15 12:29:22 +0200

    add guest caching to erdnest (#89)
    * add guest caching to erdnest
    
    * remove else and use distinct
    
    * use mixing pattern to send data to guest cache middleware
    
    * sanitize params in query check interceptor
    
    * determine cache key for caching interceptor based on request query object
    
    * add guest caching interceptor and sanitizer
    
    * use guest caching interceptor only for GET requests
    
    * added also request params in cache key
    
    * handle no cache header in caching & guest caching interceptors
    
    * add ignore authorization header flag
    
    * take into account also the NoCache decorator
    
    * moved the NoCache decorator check in the guest caching interceptor
    
    * incremented version
    
    Co-authored-by: Traian Anghel <tanghel@live.com>

--- packages/common/src/common/metrics/metrics.service.ts
+++ packages/common/src/common/metrics/metrics.service.ts
@@ -13,7 +13,11 @@ export class MetricsService {
   private static jobsHistogram: Histogram<string>;
   private static pendingApiHitGauge: Gauge<string>;
   private static cachedApiHitGauge: Gauge<string>;
+  private static guestHitsGauge: Gauge<string>;
+  private static guestNoCacheHitsGauge: Gauge<string>;
+  private static guestHitQueriesGauge: Gauge<string>;
   private static isDefaultMetricsRegistered: boolean = false;
+
 
   constructor() {
     if (!MetricsService.apiCpuTimeHistogram) {
@@ -94,6 +98,30 @@ export class MetricsService {
       });
     }
 
+    if (!MetricsService.guestNoCacheHitsGauge) {
+      MetricsService.guestNoCacheHitsGauge = new Gauge({
+        name: 'guest_no_cache_hits',
+        help: 'Request no-cache hits for guest users',
+        labelNames: [],
+      });
+    }
+
+    if (!MetricsService.guestHitsGauge) {
+      MetricsService.guestHitsGauge = new Gauge({
+        name: 'guest_hits',
+        help: 'Request hits for guest users',
+        labelNames: [],
+      });
+    }
+
+    if (!MetricsService.guestHitQueriesGauge) {
+      MetricsService.guestHitQueriesGauge = new Gauge({
+        name: 'guest_hit_queries',
+        help: 'Distinct queries for guest hit caching',
+        labelNames: [],
+      });
+    }
+
     if (!MetricsService.isDefaultMetricsRegistered) {
       MetricsService.isDefaultMetricsRegistered = true;
       collectDefaultMetrics();
@@ -135,6 +163,19 @@ export class MetricsService {
 
   incrementCachedApiHit(endpoint: string) {
     MetricsService.cachedApiHitGauge.inc({ endpoint });
+  }
+
+
+  static incrementGuestHits() {
+    MetricsService.guestHitsGauge.inc();
+  }
+
+  static incrementGuestNoCacheHits() {
+    MetricsService.guestNoCacheHitsGauge.inc();
+  }
+
+  static setGuestHitQueries(count: number) {
+    MetricsService.guestHitQueriesGauge.set(count);
   }
 
   async getMetrics(): Promise<string> {
