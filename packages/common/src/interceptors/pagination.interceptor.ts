4d09e35545e7498b4699b348020d9a37da6175f2
00ssh <42207435+00ssh@users.noreply.github.com>
2022-12-22 15:42:30 +0200

    Update pagination.interceptor.ts

--- packages/common/src/interceptors/pagination.interceptor.ts
+++ packages/common/src/interceptors/pagination.interceptor.ts
@@ -2,7 +2,7 @@ import { CallHandler, ExecutionContext, HttpException, HttpStatus, NestIntercept
 import { Observable } from "rxjs";
 
 export class PaginationInterceptor implements NestInterceptor {
-  constructor(private readonly maxSize: number = 10000) { }
+  constructor(private readonly maxSize: number = 100000) { }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const contextType: string = context.getType();
