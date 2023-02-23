5296be134724e119ef4c600aba059a5207bc1352
Traian Anghel <tanghel@live.com>
2023-02-23 20:46:12 +0100

    Fixed import (#129)
    * fixed performanceprofiler import
    
    * version bump

--- packages/common/src/guards/jwt.authenticate.guard.ts
+++ packages/common/src/guards/jwt.authenticate.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext, Inject } from '@nestjs/common';
 import { verify } from 'jsonwebtoken';
-import { PerformanceProfiler } from 'src/utils/performance.profiler';
+import { PerformanceProfiler } from '../utils/performance.profiler';
 import { ErdnestConfigService } from '../common/config/erdnest.config.service';
 import { ERDNEST_CONFIG_SERVICE } from '../utils/erdnest.constants';
 
