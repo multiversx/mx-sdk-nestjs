ba09278a80d8acef23899e0aaaa7b0bb5cec6b31
tanghel <tanghel@live.com>
2023-03-06 09:58:24 +0200

    fixed tests

--- packages/common/test/utils/address.utils.spec.ts
+++ packages/common/test/utils/address.utils.spec.ts
@@ -16,9 +16,9 @@ describe('Address utils', () => {
   });
 
   it('compute shard for address', () => {
-    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1rf4hv70arudgzus0ymnnsnc4pml0jkywg2xjvzslg0mz4nn2tg7q7k0t6p'))).toEqual(0);
-    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l'))).toEqual(4294967295);
-    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1yghjyzgq03vlmmav3cvdkcjqmnagq9u0qd7sqvt9060um88lxdrq7zs7za'))).toEqual(2);
+    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1rf4hv70arudgzus0ymnnsnc4pml0jkywg2xjvzslg0mz4nn2tg7q7k0t6p'), 3)).toEqual(0);
+    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqplllst77y4l'), 3)).toEqual(4294967295);
+    expect(AddressUtils.computeShard(AddressUtils.bech32Decode('erd1yghjyzgq03vlmmav3cvdkcjqmnagq9u0qd7sqvt9060um88lxdrq7zs7za'), 3)).toEqual(2);
   });
 
   it('check if address is valid, return true', () => {
