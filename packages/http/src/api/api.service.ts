4b934b5124bd5664447d1e09bd03fdd3af42c4bf
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2023-05-28 12:50:45 +0300

    fix general request

--- packages/http/src/api/api.service.ts
+++ packages/http/src/api/api.service.ts
@@ -194,6 +194,44 @@ export class ApiService {
     }
   }
 
+  async request(requestConfig: AxiosRequestConfig, errorHandler?: (error: any) => Promise<boolean>): Promise<any> {
+    const settings = new ApiSettings();
+
+    const profiler = new PerformanceProfiler();
+    try {
+      const config = await this.getConfig(settings);
+
+      if (!requestConfig.headers) {
+        requestConfig.headers = {};
+      }
+
+      requestConfig.headers = { ...config.headers, ...requestConfig.headers };
+      requestConfig.httpAgent = config.httpAgent;
+      requestConfig.timeout = config.timeout;
+      requestConfig.transformResponse = config.transformResponse;
+
+      const response = await axios.request(requestConfig);
+      return response;
+    } catch (error: any) {
+      let handled = false;
+      if (errorHandler) {
+        handled = await errorHandler(error);
+      }
+
+      if (!handled) {
+        const customError = this.getCustomError('REQUEST', requestConfig.url || '', requestConfig.data, error);
+
+        const logger = new Logger(ApiService.name);
+        logger.error(customError);
+
+        throw customError;
+      }
+    } finally {
+      profiler.stop();
+      this.metricsService.setExternalCall(this.getHostname(requestConfig.baseURL as string), profiler.duration);
+    }
+  }
+
   async delete(url: string, data: any, settings: ApiSettings = new ApiSettings(), errorHandler?: (error: any) => Promise<boolean>): Promise<any> {
     const profiler = new PerformanceProfiler();
     try {
@@ -242,41 +280,6 @@ export class ApiService {
         const customError = this.getCustomError('HEAD', url, null, error);
 
         const logger = new Logger(ApiService.name);
-        logger.error(customError);
-
-        throw customError;
-      }
-    } finally {
-      profiler.stop();
-      this.metricsService.setExternalCall(this.getHostname(url), profiler.duration);
-    }
-  }
-
-
-  async request(options: AxiosRequestConfig, settings: ApiSettings = new ApiSettings(), errorHandler?: (error: any) => Promise<boolean>): Promise<any> {
-    const profiler = new PerformanceProfiler();
-
-    try {
-      const config = await this.getConfig(settings);
-
-      return await axios.patch(url, data, config);
-    } catch (error: any) {
-      let handled = false;
-      if (errorHandler) {
-        handled = await errorHandler(error);
-      }
-
-      if (!handled) {
-        const logger = new Logger(ApiService.name);
-        const customError = {
-          method: 'PATCH',
-          url,
-          response: error.response?.data,
-          status: error.response?.status,
-          message: error.message,
-          name: error.name,
-        };
-
         logger.error(customError);
 
         throw customError;
