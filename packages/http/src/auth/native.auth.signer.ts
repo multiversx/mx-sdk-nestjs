683169fe3aa3ed2e21736c79eafe095eb3e96ee6
danielailie <danielailie12@gmail.com>
2023-08-17 14:24:12 +0300

    Upgrade dependencies

--- packages/http/src/auth/native.auth.signer.ts
+++ packages/http/src/auth/native.auth.signer.ts
@@ -38,11 +38,11 @@ export class NativeAuthSigner {
 
     const signableMessage = this.getSignableMessage(signerAddress, signableToken);
 
-    await userSigner.sign(signableMessage);
+    await userSigner.sign(signableMessage.serializeForSigning());
 
     const signature = signableMessage.getSignature();
 
-    const token = this.nativeAuthClient.getToken(signerAddress, signableToken, signature.hex());
+    const token = this.nativeAuthClient.getToken(signerAddress, signableToken, signature.toString('hex'));
     const expiryDate = new Date().addSeconds(this.config.expirySeconds);
 
     return this.accessTokenInfo = {
