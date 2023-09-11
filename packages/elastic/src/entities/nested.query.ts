6052f78c0c6a7de9453832e59de187cb8a9affee
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-09-11 18:03:32 +0300

    fix elastic nested query
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- packages/elastic/src/entities/nested.query.ts
+++ packages/elastic/src/entities/nested.query.ts
@@ -1,10 +1,10 @@
 import { AbstractQuery } from "./abstract.query";
-import { QueryType } from "./query.type";
+import { MatchQuery } from "./match.query";
 
 export class NestedQuery extends AbstractQuery {
   constructor(
     private readonly key: string,
-    private readonly value: any
+    private readonly value: MatchQuery[]
   ) {
     super();
   }
@@ -15,9 +15,7 @@ export class NestedQuery extends AbstractQuery {
         path: this.key,
         query: {
           bool: {
-            must: [
-              QueryType.Match(Object.keys(this.value)[0], Object.values(this.value)[0]).getQuery(),
-            ],
+            must: this.value.map((item) => item.getQuery()),
           },
         },
       },
