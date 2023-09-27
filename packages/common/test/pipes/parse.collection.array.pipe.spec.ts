70c74ba5b4c343da55316e143d6e1a314ddd8017
catalinfaurpaul <catalinfaurpaul@gmail.com>
2023-09-27 16:22:00 +0300

    add support for collection / nft identifiers pipes

---
+++ packages/common/test/pipes/parse.collection.array.pipe.spec.ts
@@ -0,0 +1,39 @@
+import { ArgumentMetadata, BadRequestException } from "@nestjs/common";
+import { ParseCollectionArrayPipe } from "../../../common/src/pipes/parse.collection.array.pipe";
+
+describe('ParseCollectionArrayPipe', () => {
+  let target: ParseCollectionArrayPipe;
+
+  beforeEach(() => {
+    target = new ParseCollectionArrayPipe();
+  });
+
+  describe('transform', () => {
+    describe('when validation passes', () => {
+      it('should return array of collection identifiers', async () => {
+        const validCollectionIdentifier = 'ABCDE-efb116';
+        expect(await target.transform(validCollectionIdentifier, {} as ArgumentMetadata)).toStrictEqual([validCollectionIdentifier]);
+      });
+
+      it('should return undefined for an empty string', async () => {
+        expect(await target.transform('', {} as ArgumentMetadata)).toBeUndefined();
+      });
+
+      it('should return undefined for undefined value', async () => {
+        expect(await target.transform(undefined, {} as ArgumentMetadata)).toBeUndefined();
+      });
+    });
+
+    describe('when validation fails', () => {
+      it('should throw BadRequestException', async () => {
+        const invalidCollectionIdentifier = 'ABCDE-efb116-02';
+        await expect(target.transform(invalidCollectionIdentifier, {} as ArgumentMetadata)).rejects.toThrow(BadRequestException);
+      });
+
+      it('should throw BadRequestException even if array contains an valid collection identifier', async () => {
+        const invalidCollectionIdentifiers = ['ABCDE-efb116-02', 'ABCDE-efb116'];
+        await expect(target.transform(invalidCollectionIdentifiers, {} as ArgumentMetadata)).rejects.toThrow(BadRequestException);
+      });
+    });
+  });
+});
---
+++ packages/common/test/pipes/parse.nft.array.pipe.spec.ts
@@ -0,0 +1,44 @@
+import { ArgumentMetadata, BadRequestException } from "@nestjs/common";
+import { ParseNftArrayPipe } from "../../../common/src/pipes/parse.nft.array.pipe";
+
+describe('ParseNftArrayPipe', () => {
+  let target: ParseNftArrayPipe;
+
+  beforeEach(() => {
+    target = new ParseNftArrayPipe();
+  });
+
+  describe('transform', () => {
+    describe('when validation passes', () => {
+      it('should return array of NFT identifiers', async () => {
+        const validNftIdentifier = 'ABCDE-efb116-02';
+        expect(await target.transform(validNftIdentifier, {} as ArgumentMetadata)).toStrictEqual([validNftIdentifier]);
+      });
+
+      it('should thorw BadRequestException even if array contains and valid identifier', async () => {
+        const validNftIdentifier = 'ABCDE-efb116-02';
+        expect(await target.transform(validNftIdentifier, {} as ArgumentMetadata)).toStrictEqual([validNftIdentifier]);
+      });
+
+      it('should return undefined for an empty string', async () => {
+        expect(await target.transform('', {} as ArgumentMetadata)).toBeUndefined();
+      });
+
+      it('should return undefined for undefined value', async () => {
+        expect(await target.transform(undefined, {} as ArgumentMetadata)).toBeUndefined();
+      });
+    });
+
+    describe('when validation fails', () => {
+      it('should throw BadRequestException', async () => {
+        const invalidNftIdentifier = 'ABCDE-efb116';
+        await expect(target.transform(invalidNftIdentifier, {} as ArgumentMetadata)).rejects.toThrow(BadRequestException);
+      });
+
+      it('should throw BadRequestException even if array contains an valid identifier', async () => {
+        const invalidNftIdentifiers = ['ABCDE-efb116-02', 'ABCDE-efb116'];
+        await expect(target.transform(invalidNftIdentifiers, {} as ArgumentMetadata)).rejects.toThrow(BadRequestException);
+      });
+    });
+  });
+});
