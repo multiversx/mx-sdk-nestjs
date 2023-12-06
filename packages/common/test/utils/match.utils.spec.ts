926135dac657c4c09102f2a59656caf4eaa95a8e
danielailie <danielailie12@gmail.com>
2023-12-06 15:06:56 +0200

    Fix tags decoding

--- packages/common/test/utils/match.utils.spec.ts
+++ packages/common/test/utils/match.utils.spec.ts
@@ -10,6 +10,11 @@ describe('Match Utils', () => {
       if (match?.groups) {
         expect(match.groups['tags']).toEqual('1,2,3,4');
       }
+      match = MatchUtils.getTagsFromBase64Attributes('dGFnczpuZnQtdGlja2V0LHJvYWQ7bWV0YWRhdGE6UW1SY1A5NGtYcjV6WmpSR3ZpN21KNnVuN0xweFVoWVZSNFI0UnBpY3h6Z1lrdA==');
+      expect(match).toBeDefined();
+      if (match?.groups) {
+        expect(match.groups['tags']).toEqual('nft-ticket,road');
+      }
     });
     it('getMetadataFromBase64Attributes', () => {
       let match = MatchUtils.getMetadataFromBase64Attributes('bWV0YWRhdGE6dGVzdDt0YWdzOjEsMiwzLDQ=');
