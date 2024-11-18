bf143d602e081347e4465ea4f2295eb028280f77
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2024-11-18 14:53:13 +0200

    API-82: tokens with prefix

--- packages/common/test/utils/token.utils.spec.ts
+++ packages/common/test/utils/token.utils.spec.ts
@@ -3,20 +3,24 @@ import { TokenUtils } from "../../src/utils/token.utils";
 describe('isToken', () => {
     it('Check isToken function', () => {
       expect(TokenUtils.isToken('MEX-455c57')).toBeTruthy();
-      expect(TokenUtils.isToken('EWLD-e23800-455c74')).toBeFalsy();
+      expect(TokenUtils.isToken('SOV-EWLD-455c74')).toBeTruthy();
+      expect(TokenUtils.isToken('PRF-SOV-e23800-455c74')).toBeFalsy();
     });
   });
 
   describe('isCollection', () => {
     it('Check isCollection function', () => {
       expect(TokenUtils.isCollection('MOS-b9b4b2')).toBeTruthy();
-      expect(TokenUtils.isCollection('MOS-b9b4b2-455c74')).toBeFalsy();
+      expect(TokenUtils.isCollection('SOV-MOS-b9b4b2')).toBeTruthy();
+      expect(TokenUtils.isCollection('SOV-MOS-b9b4b2-455c74')).toBeFalsy();
     });
   });
 
   describe('isNft', () => {
     it('Check isNft function', () => {
       expect(TokenUtils.isNft('MOS-b9b4b2-947a3912')).toBeTruthy();
+      expect(TokenUtils.isNft('SOV-MOS-b9b4b2-947a3912')).toBeTruthy();
       expect(TokenUtils.isNft('MOS-b9b4b2')).toBeFalsy();
+      expect(TokenUtils.isNft('SOV-MOS-b9b4b2')).toBeFalsy();
     });
   });
