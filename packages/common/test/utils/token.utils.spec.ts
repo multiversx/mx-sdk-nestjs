7cf274e151780c12750bf0e11faf88e32183040e
bogdan-rosianu <bogdan.rosianu@yahoo.com>
2024-11-20 12:29:26 +0200

    added extra test cases

--- packages/common/test/utils/token.utils.spec.ts
+++ packages/common/test/utils/token.utils.spec.ts
@@ -4,6 +4,9 @@ describe('isToken', () => {
     it('Check isToken function', () => {
       expect(TokenUtils.isToken('MEX-455c57')).toBeTruthy();
       expect(TokenUtils.isToken('sov-EWLD-455c74')).toBeTruthy();
+      expect(TokenUtils.isToken('')).toBeFalsy();
+      expect(TokenUtils.isToken('sov-')).toBeFalsy();
+      expect(TokenUtils.isToken('sov-EWLD-455c74-00aa')).toBeFalsy();
       expect(TokenUtils.isToken('SOV-EWLD-455c74')).toBeFalsy();
       expect(TokenUtils.isToken('sov-sov-e23800-455c74')).toBeFalsy();
     });
@@ -13,6 +16,8 @@ describe('isToken', () => {
     it('Check isCollection function', () => {
       expect(TokenUtils.isCollection('MOS-b9b4b2')).toBeTruthy();
       expect(TokenUtils.isCollection('sov-MOS-b9b4b2')).toBeTruthy();
+      expect(TokenUtils.isCollection('')).toBeFalsy();
+      expect(TokenUtils.isCollection('sov-')).toBeFalsy();
       expect(TokenUtils.isCollection('SOV-MOS-b9b4b2')).toBeFalsy();
       expect(TokenUtils.isCollection('sov-MOS-b9b4b2-455c74')).toBeFalsy();
     });
@@ -22,6 +27,8 @@ describe('isToken', () => {
     it('Check isNft function', () => {
       expect(TokenUtils.isNft('MOS-b9b4b2-947a3912')).toBeTruthy();
       expect(TokenUtils.isNft('sov-MOS-b9b4b2-947a3912')).toBeTruthy();
+      expect(TokenUtils.isNft('')).toBeFalsy();
+      expect(TokenUtils.isNft('sov-')).toBeFalsy();
       expect(TokenUtils.isNft('MOS-b9b4b2')).toBeFalsy();
       expect(TokenUtils.isNft('sov-MOS-b9b4b2')).toBeFalsy();
       expect(TokenUtils.isNft('SOV-MOS-b9b4b2')).toBeFalsy();
@@ -32,6 +39,9 @@ describe('isToken', () => {
     it('Check isSovereignIdentifier function', () => {
       expect(TokenUtils.isSovereignIdentifier('sov-MOS-b9b4b2-947a3912')).toBeTruthy();
       expect(TokenUtils.isSovereignIdentifier('sov-MOS-b9b4b2')).toBeTruthy();
+      expect(TokenUtils.isSovereignIdentifier('')).toBeFalsy();
+      expect(TokenUtils.isSovereignIdentifier('sov-')).toBeFalsy();
+      expect(TokenUtils.isSovereignIdentifier('sov-MOS-b9b4b')).toBeFalsy();
       expect(TokenUtils.isSovereignIdentifier('SOV-MOS-b9b4b2-947a3912')).toBeFalsy();
       expect(TokenUtils.isSovereignIdentifier('SOV-MOS-b9b4b2')).toBeFalsy();
       expect(TokenUtils.isSovereignIdentifier('MOS-b9b4b2-947a3912')).toBeFalsy();
