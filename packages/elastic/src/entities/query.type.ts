b8d1f6854d22e4b1c35ba2dc6057905ca56912a9
bogdan-rosianu <51945539+bogdan-rosianu@users.noreply.github.com>
2024-12-09 12:19:38 +0200

    API-334: added es script query (#247)
    * API-334: added es script query
    
    * API-334: fix query type

--- packages/elastic/src/entities/query.type.ts
+++ packages/elastic/src/entities/query.type.ts
@@ -10,6 +10,7 @@ import { ShouldQuery } from "./should.query";
 import { WildcardQuery } from "./wildcard.query";
 import { StringQuery } from "./string.query";
 import { NestedShouldQuery } from "./nested.should.query";
+import { ScriptQuery } from "./script.query";
 
 export class QueryType {
   static Match = (key: string, value: any | undefined, operator: QueryOperator | undefined = undefined): MatchQuery => {
@@ -46,5 +47,9 @@ export class QueryType {
 
   static String = (key: string | string[], value: any | undefined): StringQuery => {
     return new StringQuery(key, value);
+  };
+
+  static Script = (source: string): ScriptQuery => {
+    return new ScriptQuery(source);
   };
 }
