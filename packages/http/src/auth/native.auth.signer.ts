34279d388f6c365a9bc21dfbedf4c32f324a3080
danielailie <danielailie12@gmail.com>
2025-03-26 10:53:41 +0200

    Update sdk-core packages

--- packages/http/src/auth/native.auth.signer.ts
+++ packages/http/src/auth/native.auth.signer.ts
@@ -1,8 +1,8 @@
-import { NativeAuthClient } from '@multiversx/sdk-native-auth-client';
-import { NativeAuthClientConfig } from '@multiversx/sdk-native-auth-client/lib/src/entities/native.auth.client.config';
-import { SignableMessage, UserSigner } from '@multiversx/sdk-core';
-import { FileUtils } from '@multiversx/sdk-nestjs-common/lib/utils/file.utils';
-import '@multiversx/sdk-nestjs-common/lib/utils/extensions/date.extensions';
+import { Message, MessageComputer, UserSigner } from "@multiversx/sdk-core";
+import { NativeAuthClient } from "@multiversx/sdk-native-auth-client";
+import { NativeAuthClientConfig } from "@multiversx/sdk-native-auth-client/lib/src/entities/native.auth.client.config";
+import "@multiversx/sdk-nestjs-common/lib/utils/extensions/date.extensions";
+import { FileUtils } from "@multiversx/sdk-nestjs-common/lib/utils/file.utils";
 
 export class NativeAuthSignerConfig extends NativeAuthClientConfig {
   signerPrivateKeyPath?: string | undefined = undefined;
@@ -10,7 +10,7 @@ export class NativeAuthSignerConfig extends NativeAuthClientConfig {
 }
 
 export class AccessTokenInfo {
-  token: string = '';
+  token: string = "";
   expiryDate: Date = new Date(0);
 }
 
@@ -19,7 +19,6 @@ export class NativeAuthSigner {
   private readonly nativeAuthClient: NativeAuthClient;
   private userSigner?: UserSigner;
   private accessTokenInfo?: AccessTokenInfo;
-
   constructor(config: Partial<NativeAuthSignerConfig>) {
     this.config = Object.assign(new NativeAuthSignerConfig(), config);
     this.nativeAuthClient = new NativeAuthClient(this.config);
@@ -27,25 +26,35 @@ export class NativeAuthSigner {
 
   public async getToken(): Promise<AccessTokenInfo> {
     const currentDate = new Date().addMinutes(1);
-    if (this.accessTokenInfo && currentDate <= this.accessTokenInfo.expiryDate) {
+    if (
+      this.accessTokenInfo &&
+      currentDate <= this.accessTokenInfo.expiryDate
+    ) {
       return this.accessTokenInfo;
     }
 
     const userSigner = await this.getUserSigner();
     const signableToken = await this.getSignableToken();
-    const signerAddress = userSigner.getAddress().bech32();
+    const signerAddress = userSigner.getAddress().toBech32();
 
-    const signableMessage = this.getSignableMessage(signerAddress, signableToken);
+    const message = this.getMessage(signerAddress, signableToken);
 
-    const signature = await userSigner.sign(signableMessage.serializeForSigning());
+    const messageC = new MessageComputer();
+    const signature = await userSigner.sign(
+      messageC.computeBytesForSigning(message)
+    );
 
-    const token = this.nativeAuthClient.getToken(signerAddress, signableToken, signature.toString('hex'));
+    const token = this.nativeAuthClient.getToken(
+      signerAddress,
+      signableToken,
+      Buffer.from(signature).toString("hex")
+    );
     const expiryDate = new Date().addSeconds(this.config.expirySeconds);
 
-    return this.accessTokenInfo = {
+    return (this.accessTokenInfo = {
       token,
       expiryDate,
-    };
+    });
   }
 
   private async getUserSigner(): Promise<UserSigner> {
@@ -54,27 +63,28 @@ export class NativeAuthSigner {
     }
 
     if (this.config.privateKey) {
-      return this.userSigner = UserSigner.fromPem(this.config.privateKey);
+      return (this.userSigner = UserSigner.fromPem(this.config.privateKey));
     } else if (this.config.signerPrivateKeyPath) {
-      const pemFile = await FileUtils.readFile(this.config.signerPrivateKeyPath);
+      const pemFile = await FileUtils.readFile(
+        this.config.signerPrivateKeyPath
+      );
       const pemKey = pemFile.toString();
-      return this.userSigner = UserSigner.fromPem(pemKey);
+      return (this.userSigner = UserSigner.fromPem(pemKey));
     }
 
-    throw new Error('Missing PrivateKey and SignerPrivateKeyPath in NativeAuthSigner.');
+    throw new Error(
+      "Missing PrivateKey and SignerPrivateKeyPath in NativeAuthSigner."
+    );
   }
 
   private getSignableToken(): Promise<string> {
     return this.nativeAuthClient.initialize();
   }
 
-  private getSignableMessage(
-    signerAddress: string,
-    signableToken: string,
-  ): SignableMessage {
+  private getMessage(signerAddress: string, signableToken: string): Message {
     const messageToSign = `${signerAddress}${signableToken}`;
-    return new SignableMessage({
-      message: Buffer.from(messageToSign, 'utf8'),
+    return new Message({
+      data: Buffer.from(messageToSign, "utf8"),
     });
   }
 }
